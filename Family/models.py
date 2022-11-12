4b66160d319ce71d05ea19c11baba229b1f7977b
Miguel Mejia <analistadedatos3@farmaciasperuanas.pe>
2022-11-11 21:37:46 -0500

    update models.py

--- Family/models.py
+++ Family/models.py
@@ -6,6 +6,6 @@ class Familia(models.Model):
     apellido = models.CharField(max_length=30)
     relacion = models.CharField(max_length=30)
     edad = models.IntegerField()
-    fecha_nacimiento = models.DateField()
+    fecha_de_nacimiento = models.DateField()
     email = models.CharField(max_length=20)
     profesion = models.CharField(max_length=20)
