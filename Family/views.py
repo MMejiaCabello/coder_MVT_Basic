00656c1a123fd88df318deb0bde666f1af62c3c3
Miguel Mejia <analistadedatos3@farmaciasperuanas.pe>
2022-11-11 21:39:01 -0500

    update views.py

--- Family/views.py
+++ Family/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Familia
 
 # Create your views here.
+def inicio(request):
+    return HttpResponse('<h1>Inicio</h1>')
+
+def family(request):
+    familias = Familia.objects.all()
+    return render(request, 'family.html', {'familias':familias})
